57fa80657e2784b265f92042dcec5d6cd92a13cc
Brett Heenan <brettrheenan@gmail.com>
2017-06-11 20:25:35 +0200

    got current user working in chat channel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
+
+  def after_sign_in_path_for(resource)
+    cookies.signed[:user_id] = current_user.id
+    root_path
+  end
 end
